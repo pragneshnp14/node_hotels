d16054dbd3d6b2c6452bd28366c4018cc1784d95
pragnesh <pragneshnp1430@gmail.com>
2024-06-15 16:56:35 +0530

    Refreshed Git cache

---
+++ db.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Define the MongoDB connection URL
+const mongoURL = process.env.MONGODB_URL_LIVE;
+
+// Set up MongoDB connection with updated options
+mongoose.connect(mongoURL, {
+  tls: true, // Enforce TLS/SSL connection
+  serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
+  socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
+  retryWrites: true
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+  console.log('Connected to MongoDB server');
+});
+
+db.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+db.on('disconnected', () => {
+  console.log('MongoDB disconnected. Attempting to reconnect...');
+  mongoose.connect(mongoURL, {
+    tls: true, // Enforce TLS/SSL connection
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+    retryWrites: true
+  }).catch((err) => {
+    console.error('Error during MongoDB reconnection attempt:', err);
+  });
+});
+
+module.exports = db;
