1372c7e8c50a0479e9dadcaa0e716d578a1008ad
pragnesh <pragneshnp1430@gmail.com>
2024-06-19 18:33:09 +0530

    issues fixed

--- db.js
+++ db.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-
-// Debug logging
-mongoose.set('debug', true);
-
-// Log MongoDB URLs
 console.log('Local MongoDB URL:', process.env.MONGODB_URL_LOCAL);
 console.log('Live MongoDB URL:', process.env.MONGODB_URL_LIVE);
 
@@ -12,9 +7,7 @@ console.log('Live MongoDB URL:', process.env.MONGODB_URL_LIVE);
 const mongoURL = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URL_LIVE : process.env.MONGODB_URL_LOCAL;
 
 // Set up MongoDB connection with options
-mongoose.connect(mongoURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(mongoURL, { 
   // Other options like tls and retryWrites if needed
 }).then(() => {
   console.log('MongoDB connected');
@@ -35,8 +28,6 @@ db.on('error', (err) => {
 db.on('disconnected', () => {
   console.log('MongoDB disconnected. Attempting to reconnect...');
   mongoose.connect(mongoURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     // Other options like tls and retryWrites if needed
   }).catch((err) => {
     console.error('Error during MongoDB reconnection attempt:', err);
